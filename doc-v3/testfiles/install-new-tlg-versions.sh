3038c434851a882f99d4dfd0f300435fb44370b8
Frank Mittelbach <frank.mittelbach@latex-project.org>
2018-04-15 22:08:35 +0200

    added further documentation

--- doc-v3/testfiles/install-new-tlg-versions.sh
+++ doc-v3/testfiles/install-new-tlg-versions.sh
@@ -1,17 +1,21 @@
 
-# simple script to mov log files to tlgs (after checking them manually)
+# simple script to move log files to tlgs (after checking them manually)
 #
 # copy the logs into the testfiles dir and then run this script
+#
+# In latex2e the base engine is etex not pdftex :-( maybe we should 
+# normalize that one day ...
+
+BASEENGINE=pdftex
 
 echo ===== handling all logs  ===========
 
 for f in `ls *.xetex.log 2>/dev/null` ; do
   echo Handle FILE $f
   BASE=`basename $f '.xetex.log'`
-  cmp -s $f $BASE.pdftex.log
+  cmp -s $f $BASE.$BASEENGINE.log
   if test $? -eq 0 ; then
     echo ' => files are identical ==='
-    rm $f
     echo removing $f and $BASE.xetex.tlg
     rm -f $f $BASE.xetex.tlg
   else
@@ -23,10 +27,9 @@ done
 for f in `ls *.luatex.log 2>/dev/null` ; do
   echo Handle FILE $f
   BASE=`basename $f '.luatex.log'`
-  cmp -s $f $BASE.pdftex.log
+  cmp -s $f $BASE.$BASEENGINE.log
   if test $? -eq 0 ; then
     echo ' => files are identical ==='
-    rm $f
     echo removing $f and $BASE.luatex.tlg
     rm -f $f $BASE.luatex.tlg
   else
@@ -35,9 +38,9 @@ for f in `ls *.luatex.log 2>/dev/null` ; do
   fi
 done
 
-for f in `ls *.pdftex.log 2>/dev/null` ; do
+for f in `ls *.$BASEENGINE.log 2>/dev/null` ; do
   echo Handle FILE $f
-  mv -f $f `basename $f '.pdftex.log'`.tlg
+  mv -f $f `basename $f ".$BASEENGINE.log"`.tlg
 done
 
 #
