01e0481cba1cd40cc45b263c3236cdb4f757e170
Frank Mittelbach <frank.mittelbach@latex-project.org>
2018-08-24 14:12:55 +0200

    new code + test files (doc still needs writing)

---
+++ parskip/build.lua
@@ -0,0 +1,15 @@
+#!/usr/bin/env texlua
+
+--[[
+     For documentation and usage of the l3build system
+     see l3build.pdf
+--]]
+
+module = "parskip"
+
+typesetfiles  = {"*.tex"}
+
+checkruns     = 2
+
+kpse.set_program_name("kpsewhich")
+dofile(kpse.lookup("l3build.lua"))
