50ae30dd87ceaa40e70d9c8af4e12e79433ddc81
Frank Mittelbach <frank.mittelbach@latex-project.org>
2018-09-22 20:10:32 +0200

    l3build exe support

--- ftnright/build.lua
+++ ftnright/build.lua
@@ -11,5 +11,8 @@ typesetfiles  = {"*.tex"}
 
 checkruns     = 1
 
-kpse.set_program_name("kpsewhich")
-dofile(kpse.lookup("l3build.lua"))
+if not release_date then
+   kpse.set_program_name("kpsewhich")
+   dofile(kpse.lookup("l3build.lua"))
+end
+
