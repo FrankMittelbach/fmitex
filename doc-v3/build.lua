56c0d767a1905a091de0e047cf2225d76cc9a083
Frank Mittelbach <frank.mittelbach@latex-project.org>
2018-09-22 19:48:42 +0200

    l3build exe support

--- doc-v3/build.lua
+++ doc-v3/build.lua
@@ -11,5 +11,7 @@ typesetfiles  = {"*.tex"}
 
 checkruns     = 2
 
-kpse.set_program_name("kpsewhich")
-dofile(kpse.lookup("l3build.lua"))
+if not release_date then
+   kpse.set_program_name("kpsewhich")
+   dofile(kpse.lookup("l3build.lua"))
+end
