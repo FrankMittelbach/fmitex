c189c2309b45e3553746a01a67bc1641d2252355
Frank Mittelbach <frank.mittelbach@latex-project.org>
2018-09-22 19:21:26 +0200

    preparation for TUB article sent to kb; use l3build exe; some cleanup; test file for expl3 use

--- dashundergaps/build.lua
+++ dashundergaps/build.lua
@@ -7,9 +7,17 @@
 
 module = "dashundergaps"
 
+sourcefiles = {"*.dtx", "*.ins", "*-????-??-??.sty", "l3doc-TUB.cls"}
+
 typesetfiles  = {"*.tex"}
+
+typesetsourcefiles = {"l3doc-TUB.cls"}
 
 checkruns     = 1
 
-kpse.set_program_name("kpsewhich")
-dofile(kpse.lookup("l3build.lua"))
+
+if not release_date then
+   kpse.set_program_name("kpsewhich")
+   dofile(kpse.lookup("l3build.lua"))
+end
+
